721181efd05477aa58d11df684dbdcb5c9b47eb2
mohitsethi <mohit@sethis.in>
2014-08-26 11:27:36 +0000

    Added Support for Debian, Ubuntu, Windows

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,16 @@
 name             'chef-atom'
-maintainer       ''
-maintainer_email ''
-license          ''
-description      'Installs/Configures chef-atom'
+maintainer       'Mohit Sethi'
+maintainer_email 'mohit@sethis.in'
+license          'Apache 2.0 '
+description      'Installs/Configures Atom()'
 long_description 'Installs/Configures chef-atom'
 version          '0.1.0'
 
+recipe 'atom', 'Installs/Configures Atom'
+
+supports 'windows'
+supports 'ubuntu'
+supports 'debian'
+
+depends 'chocolatey'
+depends 'apt'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,22 @@
 # Cookbook Name:: chef-atom
 # Recipe:: default
 #
-# Copyright (C) 2014 
+# Copyright (C) 2014 Mohit Sethi <mohit@sethis.in>
 #
-# 
 #
+case node['platform_family']
+when 'windows'
+  include_recipe 'chocolatey'
+  chocolatey 'atom'
+when 'debian', 'ubuntu'
+  # Add ppa
+  include_recipe 'apt'
+  apt_repository 'atom-ppa' do
+    uri            'http://ppa.launchpad.net/webupd8team/atom/ubuntu'
+    distribution   node['lsb']['codename']
+    components     ['main']
+    keyserver      'keyserver.ubuntu.com'
+    key            'EEA14886'
+  end
+  package 'atom'
+end
