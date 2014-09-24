4196251761de721b04adf6f62adfdb2832e20f41
mohitsethi <mohit@sethis.in>
2014-09-24 06:01:31 +0000

    Bumped to v0.1.2

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mohit@sethis.in'
 license 'Apache 2.0 '
 description 'Installs/Configures Atom()'
 long_description 'Installs/Configures chef-atom'
-version '0.1.1'
+version '0.1.2'
 
 recipe 'atom', 'Installs/Configures Atom'
 
