530cf9226ab85d153f7a1375b636de90ec856fbd
MarkGibbons <gibbonsmj80@gmail.com>
2021-05-19 21:04:04 -0700

    Require version 15.3 or greater

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license           'Apache-2.0'
 source_url        'https://github.com/sous-chefs/atom'
 issues_url        'https://github.com/sous-chefs/atom/issues'
 description       'Installs/Configures the Atom text editor - a hackable editor based on Electron, maintained by GitHub'
-chef_version      '>= 13.0'
+chef_version      '>= 15.3'
 version           '0.5.0'
 
 supports 'mac_os_x'
